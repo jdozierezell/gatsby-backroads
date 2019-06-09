6c0474a5871835cc5f4d0cded701b434b242e80c
Jonathan Dozier-Ezell <jdozierezell@gmail.com>
2019-06-09 06:22:26 -0400

    initial commit

---
+++ src/constants/services.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { FaWallet, FaTree, FaSocks } from 'react-icons/fa'
+
+export default [
+    {
+        icon: <FaWallet />,
+        title: 'saving money',
+        text:
+            'Lorem dolor sit amet, consectetur adipisicing elit. Aliquam illum vitae voluptatibus.ipsum',
+    },
+    {
+        icon: <FaTree />,
+        title: 'endless hiking',
+        text:
+            'Lorem dolor sit amet, consectetur adipisicing elit. Aliquam illum vitae voluptatibus.ipsum',
+    },
+    {
+        icon: <FaSocks />,
+        title: 'amazing comfort',
+        text:
+            'Lorem dolor sit amet, consectetur adipisicing elit. Aliquam illum vitae voluptatibus.ipsum',
+    },
+]
