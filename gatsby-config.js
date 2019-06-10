7b95dc7e7a877e97a8a5b7cdbe3a883d1989c9e7
Jonathan Dozier-Ezell <jdozierezell@gmail.com>
2019-06-10 06:43:53 -0400

    lesson 57

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,26 @@
 module.exports = {
-    plugins: [`gatsby-plugin-sass`, `gatsby-plugin-styled-components`],
+    siteMetadata: {
+        title: 'BackRoads',
+        description:
+            'Explore awesome worldwide tours & discover what makes each of them unique. Forget your daily' +
+            ' routine & say yes to adventure',
+        author: 'Jonathan Dozier-Ezell',
+        data: {
+            name: 'john',
+            age: 24,
+        },
+    },
+    plugins: [
+        `gatsby-plugin-sass`,
+        `gatsby-plugin-styled-components`,
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: `${__dirname}/src/images/`,
+            },
+        },
+    ],
 }
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import Layout from '../components/Layout'
+import Images from '../examples/Images'
+
 const blog = () => {
     return (
         <Layout>
+            <Images />
             hello from blog page<Link to="/">Home</Link>
         </Layout>
     )
