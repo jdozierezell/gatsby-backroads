98f4715b57bdca35a0d00fab5e6cedcc2533831b
Jonathan Dozier-Ezell <jdozierezell@gmail.com>
2019-06-11 13:03:13 -0400

    lesson 74

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,8 +1,28 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import StyledHero from '../components/StyledHero'
+import { graphql } from 'gatsby'
+import Contact from '../components/Contact/Contact'
 
-const contact = () => {
-    return <Layout>hello from contact</Layout>
+const contact = ({ data }) => {
+    return (
+        <Layout>
+            <StyledHero img={data.contactBcg.childImageSharp.fluid} />
+            <Contact />
+        </Layout>
+    )
 }
+
+export const query = graphql`
+    query {
+        contactBcg: file(relativePath: { eq: "connectBcg.jpeg" }) {
+            childImageSharp {
+                fluid(maxWidth: 4160, quality: 90) {
+                    ...GatsbyImageSharpFluid_withWebp
+                }
+            }
+        }
+    }
+`
 
 export default contact
--- src/pages/tours.js
+++ src/pages/tours.js
@@ -1,8 +1,26 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import StyledHero from '../components/StyledHero'
+import { graphql } from 'gatsby'
 
-const tours = () => {
-    return <Layout>Hello from tours page!!!</Layout>
+const tours = ({ data }) => {
+    return (
+        <Layout>
+            <StyledHero img={data.defaultBcg.childImageSharp.fluid} />
+        </Layout>
+    )
 }
+
+export const query = graphql`
+    query {
+        defaultBcg: file(relativePath: { eq: "defaultBcg.jpeg" }) {
+            childImageSharp {
+                fluid(maxWidth: 4160, quality: 90) {
+                    ...GatsbyImageSharpFluid_withWebp
+                }
+            }
+        }
+    }
+`
 
 export default tours
